9af98b2acad1a8de05c3604cd427103397ed1e17
Dmitriy Komar <dmitriyko@playtika.com>
2021-09-01 19:53:40 +0200

    user registered method implemented

---
+++ src/main/java/com/dimkomar/jwtappdemo/service/impl/UserServiceImpl.java
@@ -0,0 +1,68 @@
+package com.dimkomar.jwtappdemo.service.impl;
+
+import com.dimkomar.jwtappdemo.model.Role;
+import com.dimkomar.jwtappdemo.model.Status;
+import com.dimkomar.jwtappdemo.model.User;
+import com.dimkomar.jwtappdemo.repository.RoleRepository;
+import com.dimkomar.jwtappdemo.repository.UserRepository;
+import com.dimkomar.jwtappdemo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    //??
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public User register(User user) {
+        Role roleUser = roleRepository.findByName("ROLE_USER");
+        List<Role> userRoles = new ArrayList<>();
+        userRoles.add(roleUser);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(userRoles);
+        user.setStatus(Status.ACTIVE);
+
+        User registeredUser = userRepository.save(user);
+        log.info("IN register - user: {} successfully registered", registeredUser);
+
+        return registeredUser;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User finByUserName(String username) {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
